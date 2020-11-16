e61b1fc6112c62b21e4d5842e1384b5dc566e9da
Francisco Cornejo <fcornejoayala@ext.copper.com>
2020-11-16 13:42:37 -0500

    Adding previewmode test

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -7,17 +7,17 @@ export default async (req, res) => {
 
   // Fetch the headless CMS to check if the provided `slug` exists
   // getPostBySlug would implement the required fetching logic to the headless CMS
-  const post = await getPostBySlug(req.query.slug)
+  //const post = await getPostBySlug(req.query.slug)
 
   // If the slug doesn't exist prevent preview mode from being enabled
-  if (!post) {
-    return res.status(401).json({ message: 'Invalid slug' })
-  }
-
+  // if (!post) {
+  //   return res.status(401).json({ message: 'Invalid slug' })
+  // }
+  console.log("yeahp");
   // Enable Preview Mode by setting the cookies
   res.setPreviewData({})
 
   // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  res.redirect(post.slug)
+  res.redirect('https://datajoy.vercel.app/')
 }
