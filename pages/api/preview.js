c6d0f3605189f609a8c9e1f093a7924c2171d8ee
Francisco Cornejo <fcornejoayala@ext.copper.com>
2020-11-17 13:14:55 -0500

    test

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,23 +1,37 @@
-export default async (req, res) => {
+import { getPreviewPost } from '../../lib/api'
+
+export default async function preview(req, res) {
+  const { secret, id, slug } = req.query
+
   // Check the secret and next parameters
-  // This secret should only be known to this API route and the CMS
-  if (req.query.secret !== 'dataJ0yW3b' || !req.query.slug) {
+  // This secret should only be known by this API route
+  if (
+    !process.env.WORDPRESS_PREVIEW_SECRET ||
+    secret !== process.env.WORDPRESS_PREVIEW_SECRET ||
+    (!id && !slug)
+  ) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
-  // Fetch the headless CMS to check if the provided `slug` exists
-  // getPostBySlug would implement the required fetching logic to the headless CMS
-  //const post = await getPostBySlug(req.query.slug)
+  // Fetch WordPress to check if the provided `id` or `slug` exists
+  const post = await getPreviewPost(id || slug, id ? 'DATABASE_ID' : 'SLUG')
 
-  // If the slug doesn't exist prevent preview mode from being enabled
-  // if (!post) {
-  //   return res.status(401).json({ message: 'Invalid slug' })
-  // }
-  console.log("yeahp");
+  // If the post doesn't exist prevent preview mode from being enabled
+  if (!post) {
+    return res.status(401).json({ message: 'Post not found' })
+  }
+
   // Enable Preview Mode by setting the cookies
-  res.setPreviewData({})
+  res.setPreviewData({
+    post: {
+      id: post.databaseId,
+      slug: post.slug,
+      status: post.status,
+    },
+  })
 
   // Redirect to the path from the fetched post
-  // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  res.redirect(`https://datajoy.vercel.app/${req.query.slug}`)
+  // We don't redirect to `req.query.slug` as that might lead to open redirect vulnerabilities
+  res.writeHead(307, { Location: `/${post.slug || post.databaseId}` })
+  res.end()
 }
