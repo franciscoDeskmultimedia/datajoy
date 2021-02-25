a07abf5c570a702d16c4a5afceb0860272ae709d
Francisco Cornejo <fcornejoayala@ext.copper.com>
2021-02-24 23:08:38 -0500

    revert no limit height on full image two col component

--- components/TwoColImage/TwoColImage.js
+++ components/TwoColImage/TwoColImage.js
@@ -10,7 +10,7 @@ const TwoColImage = (props) => {
       }`}
     >
       {props.fullImage == true ? (
-        <div className="relative flex items-center justify-center w-full p-16 border-b border-black lg:border-t-0 lg:border-b-0 fullImage bg-lightblue-500 lg:w-1/2">
+        <div className="relative flex items-center justify-center w-full p-16 border-b border-black lg:border-t-0 lg:border-b-0 fullImage bg-lightblue-500 lg:h-56 lg:w-1/2">
           <Image layout="fill" src={props.imageSrc} alt={props.imageAlt} />
         </div>
       ) : (
